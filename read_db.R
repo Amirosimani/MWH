7156eadcb9e26ddd88948b08e80ddc3110522eae
amirosimani <amirhs.imani@gmail.com>
2015-11-23 21:13:40 -0500

    parsing date

--- read_db.R
+++ read_db.R
@@ -1,5 +1,8 @@
+library(stats)
 library(dplyr)
 library(RSQLite)
+library(lubridate)
+library(ggplot2)
 
 #Set up connection to the SQLite database
 connection <- dbConnect(RSQLite::SQLite(), dbname = "clinton.sqlite")
@@ -15,5 +18,24 @@ print("Column Name")
 print(colnames(docs))
 print(sprintf("Number of Rows: %d", nrow(docs)))
 
+
+#parsing date
+for (i in 1:nrow(docs)) {
+  
+  
+  arrive <- ymd_hms(docs$date, tz = "Pacific/Auckland")
+  time <- hour(arrive)
+  time <-time[complete.cases(time)]
+  
+  date <- as.Date(as.POSIXct(strptime(docs$date, "%Y-%m-%d %H:%M:%S")))
+  date[complete.cases(date)]
+  date <-date[complete.cases(date)]
+  
+  stripped_date = data.frame(date, time)
+}
+
+
+
 #Clean up connection to the database
 dbDisconnect(connection)
+
